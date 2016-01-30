3bb84d8fb753e465ff2f7be8689b5a2b5b3ae91b
admin <245809743@qq.com>
2016-01-30 16:15:20 +0800

    修改了架构  去除了swagger

--- src/main/java/com/simpleApp/controller/UserController.java
+++ src/main/java/com/simpleApp/controller/UserController.java
@@ -5,8 +5,13 @@ import com.simpleApp.service.UserService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zl on 2015/8/27.
@@ -27,6 +32,34 @@ public class UserController {
             System.out.println("user.getName():"+user.getName());
             logger.info("user.getAge():"+user.getAge());
         }
+        return user;
+    }
+
+    @RequestMapping("test")
+    public String test (){
+        return "index" ;
+    }
+
+    @RequestMapping(method= RequestMethod.GET)
+    @ResponseBody
+    public List<User> getUsers(){
+        List<User> list=new ArrayList<User>();
+
+        User user=new User();
+        user.setName("hello");
+        list.add(user);
+
+        User user2=new User();
+        user2.setName("world");
+        list.add(user2);
+        return list;
+    }
+
+    @RequestMapping(value="/{name}",method=RequestMethod.GET)
+    @ResponseBody
+    public User getUserById(@PathVariable String name){
+        User user=new User();
+        user.setName("hello world");
         return user;
     }
 }
--- src/main/java/com/simpleApp/mapper/UserMapper.java
+++ src/main/java/com/simpleApp/mapper/UserMapper.java
@@ -2,10 +2,11 @@ package com.simpleApp.mapper;
 
 
 import com.simpleApp.model.User;
+import com.simpleApp.util.MyMapper;
 
 /**
  * Created by zl on 2015/8/27.
  */
-public interface UserMapper {
+public interface UserMapper extends MyMapper<User> {
     public User findUserInfo();
 }
